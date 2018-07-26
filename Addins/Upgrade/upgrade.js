7ad98271788cb09934d3ecb5c91c16e9a40978fa
Zlatko Michailov <zlatkom@microsoft.com>
2018-07-26 15:11:44 -0700

    Upgrade add-in.

---
+++ Addins/Upgrade/upgrade.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../Office.Runtime.js" />
+/// <reference path="../Excel.js" />
+
+function version() {
+	return "18.7.24.2";
+}
+
+CustomFunctionMappings = {
+	VERSION_SYNC: version,
+	VERSION_ASYNC: version
+};
