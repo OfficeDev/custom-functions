168cc997ff99d30174fa36ed0783b2b37b0b7f83
Zlatko Michailov <zlatkom@microsoft.com>
2018-09-10 11:30:57 -0700

    Add SLEEP().

--- addins/upgrade/upgrade.js
+++ addins/upgrade/upgrade.js
@@ -5,7 +5,16 @@ function version() {
 	return "18.7.24.2";
 }
 
+function sleep(ms) {
+	return new Promise(function(resolve) {
+		setTimeout(function() {
+			resolve(version());
+		}, ms);
+	});
+}
+
 CustomFunctionMappings = {
 	VERSION_SYNC: version,
-	VERSION_ASYNC: version
+	VERSION_ASYNC: version,
+	SLEEP: sleep
 };
