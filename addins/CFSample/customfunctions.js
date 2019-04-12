b46f4df01fe8b9cfd2d65a7e64d1658bc7bc0290
Shreedhar T <shreedhar.t@hotmail.com>
2019-04-12 11:45:49 -0700

    Initial checkin of CFSample after trimming unnecessary things.

---
+++ addins/CFSample/customfunctions.js
@@ -0,0 +1,298 @@
+﻿// Returns a number
+function add42(num1, num2) {
+    return num1 + num2 + 42;
+}
+
+// Returns a string
+function getDay() {
+    var d = new Date();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return days[d.getDay()];
+}
+
+// Returns a boolean
+function isPrime(num) {
+    if (num <= 1 || !Number.isInteger(num)) return false; // validates input
+    // checks natural numbers below the square root (not optimal)
+    for (var factor = Math.floor(Math.sqrt(num)); factor > 1; factor--) {
+        if (num % factor === 0) return false;
+    }
+    return true;
+}
+
+// Computation-intensive for high inputs
+function nthPrime(n) {
+    var primeCount = 0;
+    for (var num = 2; primeCount < n; num++) {
+        if (isPrime(num)) primeCount++;
+    }
+    return num - 1;
+}
+
+// Simulate calling an external webservice to calculate result
+function add42Promise(num1, num2) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve(num1 + num2 + 42);
+        }, 1000);
+    });
+}
+
+
+
+
+// Range input
+function secondHighest(range) {
+    var highest = range[0][0], secondHighest = range[0][0];
+
+    for (var i = 0; i < range.length; i++) {
+        for (var j = 0; j < range[i].length; j++) {
+            if (range[i][j] >= highest) {
+                secondHighest = highest;
+                highest = range[i][j];
+            }
+            else if (range[i][j] >= secondHighest) {
+                secondHighest = range[i][j];
+            }
+        }
+    }
+
+    return secondHighest;
+}
+
+// Range output
+function makeArray(rows, columns) {
+    var items = new Array(rows);
+    for (var i = 0; i < rows; i++) {
+        items[i] = new Array(columns);
+        for (var j = 0; j < columns; j++) {
+            items[i][j] = i + j;
+        }
+    }
+    return items;
+}
+
+
+
+// Optional parameter
+function checkOptionalParam(requiredParam, optionalParam) {
+    if (optionalParam === null) {
+        return "Optional parameter is NOT passed in";
+    }
+    else {
+        return "Optional parameter is passed in " + optionalParam;
+    }
+}
+
+
+
+// Volatile option
+function rand(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Streaming and cancelable option
+function incrementStream(increment, caller) {
+    var result = 0;
+
+    var myInterval = setInterval(function () {
+        result += increment;
+        caller.setResult(result);
+    }, 1000);
+
+    caller.onCanceled = function () {
+        clearInterval(myInterval);
+    };
+}
+
+// requiresAddress option
+function cellAddress(caller) {
+    return caller.address;
+}
+
+
+// Write to asyncStorage
+function setAsyncStorage(key, value) {
+    return OfficeRuntime.AsyncStorage.setItem(key, value).then(function (result) {
+        return "Success: Item with key '" + key + "' saved to AsyncStorage.";
+    }, function (error) {
+        return "Error: Unable to save item with key '" + key + "' to AsyncStorage. " + error;
+    });
+}
+
+// Read from asyncStorage
+function getAsyncStorage(key) {
+    return OfficeRuntime.AsyncStorage.getItem(key);
+}
+
+// XMLHttpRequest API
+function translate(text, langLocale) {
+    return new Promise(function (resolve) {
+        var xhr = new XMLHttpRequest();
+        var textStr = encodeURIComponent(text);
+        var localeStr = encodeURIComponent(langLocale);
+
+        var url = "https://excelcf-demo-api.azurewebsites.net/api/translate?code=F69Va5ojUPvfnat9udiM8OpEcScy/oK3bV8/wBYW8OXlypR3nyV/AA==&name=" +
+            textStr + "&locale=" + localeStr;
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                resolve(xhr.responseText);
+            }
+        };
+
+        xhr.open('GET', url, true);
+        xhr.send();
+    });
+}
+
+// XMLHttpRequest API
+function stockPrice(ticker) {
+    return new Promise(
+        function (resolve) {
+            let xhr = new XMLHttpRequest();
+            let url = "https://api.iextrading.com/1.0/stock/" + ticker + "/price"
+            //add handler for xhr
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    //return result back to Excel
+
+                    resolve(xhr.responseText);
+                }
+            };
+            //make request
+
+            xhr.open('GET', url, true);
+            xhr.send();
+        });
+}
+
+// XMLHttpRequest API
+function stockPriceStream(ticker, caller) {
+
+    let result = 0;
+
+    setInterval(function () {
+        let xhr = new XMLHttpRequest();
+        let url = "https://api.iextrading.com/1.0/stock/" + ticker + "/price";
+        //add handler for xhr
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                //return result back to Excel
+
+                caller.setResult(xhr.responseText);
+            }
+        };
+
+        xhr.open('GET', url, true);
+        xhr.send();
+    }, 1000); //milliseconds
+}
+
+// fetch API
+function stockPriceHistory(ticker, days) {
+    return new Promise(function (resolve) {
+        var url = "https://excelcf-demo-api.azurewebsites.net/api/StockHistory?stock=" + ticker + "&days=" + days;
+
+        fetch(url)
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (text) {
+                resolve(JSON.parse(text));
+            })
+            .catch(function (error) {
+                //will show #VALUE!
+                console.log(error);
+                resolve(error);
+            });
+    });
+}
+
+// displayWebDialog API
+function displayWebDialog() {
+    OfficeRuntime.displayWebDialog("https://cfsample.azurewebsites.net/home.html", {
+        height: "50",
+        width: "50",
+        hideTitle: false,
+    });
+}
+
+// WebSocket API
+function bitcoinStream(increment, handler) {
+    const socket = new WebSocket('wss://ws-feed.pro.coinbase.com');
+    var isOpen = 0;
+    var counter = increment;
+
+    socket.onmessage = function (event) {
+        counter++;
+        if (counter < 1000) {
+            handler.setResult("Event Counter:" + counter + " Data:" + event.data);
+        }
+        else {
+            socket.close();
+        }
+    };
+
+    socket.onclose = function () {
+        isOpen = 0;
+        handler.setResult("Socket closed after Event Counter:" + counter);
+    };
+
+    socket.onerror = function () {
+        isOpen = 0;
+        handler.setResult("Error after Event Counter:" + counter);
+        socket.close();
+    };
+
+    socket.onopen = function () {
+        console.log("onopen");
+        socket.send(
+            JSON.stringify({
+                "type": "subscribe",
+                "product_ids": [
+                    "ETH-USD",
+                    "ETH-EUR"
+                ],
+                "channels": [
+                    "level2",
+                    "heartbeat",
+                    {
+                        "name": "ticker",
+                        "product_ids": [
+                            "ETH-BTC",
+                            "ETH-USD"
+                        ]
+                    }
+                ]
+            }
+            ));
+        isOpen = 1;
+    };
+}
+
+CustomFunctions.associate('ADD42', add42);
+CustomFunctions.associate('GET_DAY', getDay);
+CustomFunctions.associate('IS_PRIME', isPrime);
+CustomFunctions.associate('NTH_PRIME', nthPrime);
+CustomFunctions.associate('ADD42_PROMISE', add42Promise);
+CustomFunctions.associate('SECOND_HIGHEST', secondHighest);
+CustomFunctions.associate('MAKE_ARRAY', makeArray);
+CustomFunctions.associate('CHECK_OPTIONAL_PARAM', checkOptionalParam);
+CustomFunctions.associate('RAND', rand);
+CustomFunctions.associate('INCREMENT_STREAM', incrementStream);
+CustomFunctions.associate('CELL_ADDRESS', cellAddress);
+CustomFunctions.associate('SET_ASYNC_STORAGE', setAsyncStorage);
+CustomFunctions.associate('GET_ASYNC_STORAGE', getAsyncStorage);
+CustomFunctions.associate('TRANSLATE', translate);
+CustomFunctions.associate('STOCK_PRICE', stockPrice);
+CustomFunctions.associate('STOCK_PRICE_STREAM', stockPriceStream);
+CustomFunctions.associate('STOCK_PRICE_HISTORY', stockPriceHistory);
+CustomFunctions.associate('DISPLAY_WEB_DIALOG', displayWebDialog);
+CustomFunctions.associate('BITCOIN_STREAM', bitcoinStream);
+
+
+
+
