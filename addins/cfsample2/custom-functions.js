9d459a663d2cd96d7f76f8a4aabd2b48e02090bf
shaofengzhu <shaozhu@microsoft.com>
2019-11-06 22:23:58 -0800

    Use the OfficeRuntime.currentTaskpane API (#36)
    * Update CFSample2 using new API
    
    * Add logging
    
    * Reset MutationObserver instead of using promiseWorkaround

--- addins/cfsample2/custom-functions.js
+++ addins/cfsample2/custom-functions.js
@@ -1,101 +1,72 @@
-﻿function promiseWorkaround(func) {
-	var timerId;
-	if (typeof(document) !== 'undefined' && document.createElement){
-		timerId = setInterval(function(){
-			var div = document.createElement('div');
-			div.innerHTML = 'Promise Workaround';
-			div.style.display = 'none';
-			document.body.appendChild(div);
-		}, 20);
+﻿
+function SetRuntimeVisibleHelper(visible) {
+	var p;
+	if (visible) {
+		p = OfficeRuntime.currentTaskpane.show();
+	}
+	else {
+		p = OfficeRuntime.currentTaskpane.hide();
 	}
 
-	return func()
-		.then(function(value) {
-			if (timerId) {
-				clearInterval(timerId);
-			}
-
-			return value;
+	return p.then(function () {
+			return visible;
+		})
+		.catch(function (error) {
+			return error.code;
 		});
 }
 
-function SetRuntimeStateHelper(state) {
-	return promiseWorkaround(function () {
-		return OfficeRuntime.currentRuntime.setState(state)
-			.then(function () {
-				if (typeof(g_BgAppRuntimeState) === 'object') {
-					g_BgAppRuntimeState.value = state;
-				}
-				return state;
-			})
-			.catch(function (error) {
-				return error.code;
-			});
-	});
-}
-
-function SetStartupStateHelper(state) {
-	return promiseWorkaround(function () {
-		return OfficeRuntime.currentRuntime.setStartupState(state)
-			.then(function () {
-				if (typeof(g_BgAppRuntimeStartupState) === 'object') {
-					g_BgAppRuntimeStartupState.value = state;
-				}
-				return state;
-			})
-			.catch(function (error) {
-				return error.code;
-			});
-	});
+function SetStartupBehaviorHelper(state) {
+	return OfficeRuntime.currentTaskpane.setStartupBehavior(state)
+		.then(function () {
+			return state;
+		})
+		.catch(function (error) {
+			return error.code;
+		});
 }
 
 CustomFunctions.associate('GetCFDataRangeValue', function(address){
-	return promiseWorkaround(function() {
-		var context = new Excel.RequestContext();
-		var sheet = context.workbook.worksheets.getItemOrNullObject("CFData");
-		var range;
-		return context.sync()
-			.then(function() {
-				if (sheet.isNullObject) {
-					sheet = context.workbook.worksheets.add("CFData");
-				}
-				return context.sync();
-			})
-			.then(function() {
-				range = sheet.getRange(address);
-				range.load();
-				return context.sync();
-			})
-			.then(function() {
-				return range.values[0][0];
-			});
-	});
+	var context = new Excel.RequestContext();
+	var sheet = context.workbook.worksheets.getItemOrNullObject("CFData");
+	var range;
+	return context.sync()
+		.then(function() {
+			if (sheet.isNullObject) {
+				sheet = context.workbook.worksheets.add("CFData");
+			}
+			return context.sync();
+		})
+		.then(function() {
+			range = sheet.getRange(address);
+			range.load();
+			return context.sync();
+		})
+		.then(function() {
+			return range.values[0][0];
+		});
 });
 
 CustomFunctions.associate('GetRangeValue', function(address){
-	return promiseWorkaround(function() {
-		var context = new Excel.RequestContext();
-		var range = context.workbook.worksheets.getActiveWorksheet().getRange(address);
-		range.load();
-		return context.sync()
-			.then(function() {
-				return range.values[0][0];
-			});
-	});
+	var context = new Excel.RequestContext();
+	var range = context.workbook.worksheets.getActiveWorksheet().getRange(address);
+	range.load();
+	return context.sync()
+		.then(function() {
+			return range.values[0][0];
+		});
 });
 
 
 CustomFunctions.associate('SetRangeValue', function(address, value){
-	return promiseWorkaround(function() {
-		var context = new Excel.RequestContext();
-		var range = context.workbook.worksheets.getActiveWorksheet().getRange(address);
-		range.values = [[value]];
-		range.load();
-		return context.sync()
-			.then(function() {
-				return range.values[0][0];
-			});
-	});
+	var context = new Excel.RequestContext();
+	var range = context.workbook.worksheets.getActiveWorksheet().getRange(address);
+	range.values = [[value]];
+	range.load();
+	return context.sync()
+		.then(function() {
+			return range.values[0][0];
+		});
 });
 
 CustomFunctions.associate('GetValue', function() {
@@ -115,41 +86,42 @@ CustomFunctions.associate('SetValue', function(value) {
 });
 
 CustomFunctions.associate('GetRuntimeState', function() {
-	return promiseWorkaround(function () {
-		return OfficeRuntime.currentRuntime.getState()
-		.then(function (value) {
-			if (typeof(g_BgAppRuntimeState) === 'object') {
-				g_BgAppRuntimeState.value = value;
-			}
-			return value.toString();
-		})
-		.catch(function (error) {
-			return error.code;
-		});
+	// _getState() is the internal API and it's only for Microsoft engineer team internal testing purpose. Please do not use it.
+	return OfficeRuntime.currentTaskpane._getState().then(function (value) {
+		return value;
+	})
+	.catch(function (error) {
+		return error.code;
 	});
 });
 
-CustomFunctions.associate('SetRuntimeState', function (state) {
-	return SetRuntimeStateHelper(state);
+CustomFunctions.associate('GetVisibility', function() {
+	return g_BgAppVisibilityState.value;
 });
 
-CustomFunctions.associate('GetStartupState', function() {
-	return promiseWorkaround(function () {
-		return OfficeRuntime.currentRuntime.getStartupState()
-		.then(function (value) {
-			if (typeof(g_BgAppRuntimeStartupState) === 'object') {
-				g_BgAppRuntimeStartupState.value = value;
-			}
-			return value.toString();
-		})
-		.catch(function (error) {
-			return error.code;
-		});
+CustomFunctions.associate('Show', function () {
+	return SetRuntimeVisibleHelper(true);
+});
+
+CustomFunctions.associate('Hide', function () {
+	return SetRuntimeVisibleHelper(false);
+});
+
+CustomFunctions.associate('GetStartupBehavior', function() {
+	return OfficeRuntime.currentTaskpane.getStartupBehavior()
+	.then(function (value) {
+		if (typeof(g_BgAppRuntimeStartupState) === 'object') {
+			g_BgAppRuntimeStartupState.value = value;
+		}
+		return value.toString();
+	})
+	.catch(function (error) {
+		return error.code;
 	});
 });
 
 
 
-CustomFunctions.associate('SetStartupState', function (state) {
-	return SetStartupStateHelper(state);
+CustomFunctions.associate('SetStartupBehavior', function (behavior) {
+	return SetStartupBehaviorHelper(behavior);
 });
