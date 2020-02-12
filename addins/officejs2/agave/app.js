db6acddd05244ea9d28356af88de570da1d72118
shaofengzhu <shaozhu@microsoft.com>
2020-02-12 08:30:51 -0800

    Log OSFPerformance (#59)
    * Add officejs2
    
    * Update to min version of JS
    
    * Use mini version of JS
    
    * Split it to two addins
    
    * Fix filename
    
    * Update perf data
    
    * Add addin using mini js
    
    * Update excel.bundle.js and word.bundle.js to support telemetry
    
    * Rename excel.bundle.js to excel.js
    
    * update oteljs
    
    * console.error if otel.js cannot be loaded
    
    * Add getAppContext perf mark
    
    * log OSFPerformance

--- addins/officejs2/agave/app.js
+++ addins/officejs2/agave/app.js
@@ -22,6 +22,11 @@ Office.onReady(function (hostAndPlatform) {
     _perfData.officeOnReadyApp = Date.now();
     _perfData.officeOnReadyAppDuration = _perfData.officeOnReadyApp - _perfData.start;
     log(JSON.stringify(_perfData));
+    
+    if (typeof(OSFPerformance) !== 'undefined') {
+        log('OSFPerformance');
+        log(JSON.stringify(OSFPerformance));
+    }
 
     log(Office.context.displayLanguage);
     log(hostAndPlatform.host);
