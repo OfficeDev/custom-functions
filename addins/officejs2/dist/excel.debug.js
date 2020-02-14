ed4aac5417cd079f1eac43a1725d2b621a4e8e9b
Shaofeng Zhu <shaozhu@microsoft.com>
2020-02-14 12:50:49 -0800

    Fix uglified JS on Win32

--- addins/officejs2/dist/excel.debug.js
+++ addins/officejs2/dist/excel.debug.js
@@ -15,42 +15,6 @@
  */
 
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -1659,45 +1623,37 @@ var OSF;
         var _asyncMethodExecutor;
         var _officeAppContext;
         function bootstrap() {
-            return __awaiter(this, void 0, void 0, function () {
-                var officeAppContext, appNameNumber;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            _webAppState = {
-                                id: null,
-                                webAppUrl: null,
-                                conversationID: null,
-                                clientEndPoint: null,
-                                wnd: window.parent,
-                                focused: false,
-                                serviceEndPoint: null,
-                                serializerVersion: 1
-                            };
-                            retrieveHostInfo();
-                            retrieveLoggingAllowed();
-                            createInitializationHelper();
-                            if (_hostInfo.hostPlatform === OSF.HostInfoPlatform.web) {
-                                _initializationHelper.saveAndSetDialogInfo(OSF.Utility.getQueryStringValue("_host_Info"));
-                            }
-                            _initializationHelper.setAgaveHostCommunication();
-                            OSFPerformance.getAppContextStart = OSFPerformance.now();
-                            return [4, _initializationHelper.getAppContext(window)];
-                        case 1:
-                            officeAppContext = _a.sent();
-                            OSFPerformance.getAppContextEnd = OSFPerformance.now();
-                            _officeAppContext = officeAppContext;
-                            _initializationHelper.createClientHostController();
-                            _asyncMethodExecutor = _initializationHelper.createAsyncMethodExecutor();
-                            _initializationHelper.prepareApiSurface(officeAppContext);
-                            appNameNumber = officeAppContext.get_appName();
-                            Office.fireOnReady({
-                                host: OSF.HostName.Host.getInstance().getHost(appNameNumber),
-                                platform: OSF.HostName.Host.getInstance().getPlatform(appNameNumber)
-                            });
-                            return [2, officeAppContext];
-                    }
+            _webAppState = {
+                id: null,
+                webAppUrl: null,
+                conversationID: null,
+                clientEndPoint: null,
+                wnd: window.parent,
+                focused: false,
+                serviceEndPoint: null,
+                serializerVersion: 1
+            };
+            retrieveHostInfo();
+            retrieveLoggingAllowed();
+            createInitializationHelper();
+            if (_hostInfo.hostPlatform === OSF.HostInfoPlatform.web) {
+                _initializationHelper.saveAndSetDialogInfo(OSF.Utility.getQueryStringValue("_host_Info"));
+            }
+            _initializationHelper.setAgaveHostCommunication();
+            OSFPerformance.getAppContextStart = OSFPerformance.now();
+            return _initializationHelper.getAppContext(window)
+                .then(function (officeAppContext) {
+                OSFPerformance.getAppContextEnd = OSFPerformance.now();
+                _officeAppContext = officeAppContext;
+                _initializationHelper.createClientHostController();
+                _asyncMethodExecutor = _initializationHelper.createAsyncMethodExecutor();
+                _initializationHelper.prepareApiSurface(officeAppContext);
+                var appNameNumber = officeAppContext.get_appName();
+                Office.fireOnReady({
+                    host: OSF.HostName.Host.getInstance().getHost(appNameNumber),
+                    platform: OSF.HostName.Host.getInstance().getPlatform(appNameNumber)
                 });
+                return officeAppContext;
             });
         }
         _OfficeAppFactory.bootstrap = bootstrap;
@@ -3360,17 +3316,17 @@ var OSF;
             if (!this._osfControlContext) {
                 var warningText = "Warning: Office.js is loaded outside of Office client";
                 try {
-                    if (window.external && typeof window.external.GetContext !== 'undefined') {
+                    if (window.external) {
                         this._osfControlContext = window.external.GetContext();
                     }
                     else {
                         OSF.Utility.trace(warningText);
-                        return;
+                        return null;
                     }
                 }
                 catch (e) {
                     OSF.Utility.trace(warningText);
-                    return;
+                    return null;
                 }
             }
             return this._osfControlContext;
@@ -3379,7 +3335,7 @@ var OSF;
             var _this = this;
             var context = this.getOsfControlContext();
             if (!context) {
-                return Promise.resolve(null);
+                return Promise.reject(new Error("The Office.js is loaded outside of Office client"));
             }
             var appType = context.GetAppType();
             var id = context.GetSolutionRef();
@@ -3787,9 +3743,16 @@ var OSF;
         }
         Utility.debugLog = debugLog;
         function createPromiseFromResult(result) {
-            return Promise.resolve(result);
+            return createPromise(function (resolve, reject) {
+                resolve(result);
+            });
         }
         Utility.createPromiseFromResult = createPromiseFromResult;
+        function createPromise(executor) {
+            var ret = new Promise(executor);
+            return ret;
+        }
+        Utility.createPromise = createPromise;
         function compareVersions(version1, version2) {
             var splitVersion1 = version1.split(".");
             var splitVersion2 = version2.split(".");
@@ -5357,7 +5320,10 @@ var OSF;
 })(OSF || (OSF = {}));
 var OSF;
 (function (OSF) {
-    OSF._OfficeAppFactory.bootstrap();
+    OSF._OfficeAppFactory.bootstrap()
+        .catch(function (e) {
+        console.error(JSON.stringify(e));
+    });
     window.addEventListener('DOMContentLoaded', function (event) {
         OSFPerformance.hostSpecificFileName = OSF.LoadScriptHelper.getHostBundleJsName();
         Office.onReadyInternal(function () {
