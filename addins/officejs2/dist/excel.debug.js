6bfd3568583f76a21376fac7225614824076d4ee
Shaofeng Zhu <shaozhu@microsoft.com>
2020-02-13 21:29:38 -0800

    Use uglify-js for minimization

--- addins/officejs2/dist/excel.debug.js
+++ addins/officejs2/dist/excel.debug.js
@@ -1,4 +1,20 @@
-// This is generated file
+
+/*
+ * Office JavaScript API library
+ *
+ * Copyright (c) Microsoft Corporation.  All rights reserved.
+ *
+ * Your use of this file is governed by the Microsoft Services Agreement http://go.microsoft.com/fwlink/?LinkId=266419.
+ *
+ * This file also contains the following Promise implementation (with a few small modifications):
+ *      * @overview es6-promise - a tiny implementation of Promises/A+.
+ *      * @copyright Copyright (c) 2014 Yehuda Katz, Tom Dale, Stefan Penner and contributors (Conversion to ES6 API by Jake Archibald)
+ *      * @license   Licensed under MIT license
+ *      *            See https://raw.githubusercontent.com/jakearchibald/es6-promise/master/LICENSE
+ *      * @version   2.3.0
+ */
+
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
